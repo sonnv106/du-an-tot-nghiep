e78247d725ca49c6399ea6db4c8c797bca98ace3
Glitch (flying-blossom-cerise) <none>
2022-03-29 03:29:48 +0000

    Cập nhật api update toàn bộ giỏ hàng

--- api/controllers/cart.controller.js
+++ api/controllers/cart.controller.js
@@ -1,4 +1,3 @@
-
 var Category = require("../../models/category.model");
 var Product = require("../../models/product.model");
 var User = require("../../models/user.model");
@@ -66,17 +65,28 @@ module.exports.addToCart = async (req, res) => {
 
 //update toan bo gio hang
 module.exports.update = async (req, res) => {
-  var user_id = jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET).id;
-  var cart = await Cart.findOne({ user_id: user_id });
-  cart.products = req.body.products;
-  var sum = [];
-  for (var i = 0; i < cart.products.length; i++) {
-    sum.push(cart.products[i].price * cart.products[i].amount);
+  if (req.body.token && req.body.products) {
+    var user_id = jwt.verify(
+      req.body.token,
+      process.env.ACCESS_TOKEN_SECRET
+    ).id;
+    var cart = await Cart.findOne({ user_id: user_id });
+
+    cart.products = req.body.products;
+    await cart.save();
+    var sum = [];
+    for (var i = 0; i < cart.products.length; i++) {
+      sum.push(cart.products[i].price * cart.products[i].amount);
+    }
+    var total = sum.reduce((a, b) => {
+      return a + b;
+    }, 0);
+    cart.total = total;
+    console.log(cart);
+    await cart.save();
+    res.json(cart);
+  }else{
+    res.json('erros')
+    return;
   }
-  var total = sum.reduce((a, b) => {
-    return a + b;
-  }, 0);
-  cart.total = total;
-  await cart.save();
-  res.json(cart);
 };
